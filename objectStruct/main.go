6e7bf67660bc82e2a0186645838d6dd771ff13d6
Shokin Ilya <atlus3@mail.ru>
2018-08-11 23:56:30 +0300

    разные тесты со структурой и методами

---
+++ objectStruct/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// тестовая структура
+type objStruct struct {
+	X, Y float64
+	Name string `json:"structName"`
+}
+
+// передвижение объекта
+func (obj *objStruct) move(lenX, lenY float64) {
+	obj.X += lenX
+	obj.X += lenY
+}
+
+// метод имя
+func (obj *objStruct) sayName() {
+	fmt.Println(obj.Name)
+}
+
+// json объекта
+func (obj objStruct) toJSON() string {
+
+	a := &objStruct{obj.X, obj.Y, obj.Name}
+	out, err := json.Marshal(a)
+	if err != nil {
+		panic(err)
+	}
+	// fmt.Println(out)
+
+	return string(out)
+}
+
+func main() {
+
+	mover := objStruct{X: 10.0, Y: 10.0}
+
+	mover.Name = "Jack"
+	mover.move(2.0, 1)
+
+	jsonStr := mover.toJSON()
+	fmt.Println(jsonStr)
+
+	// fmt.Println(string(mover.toJSON()))
+
+	// for field, value := range mover {
+	// 	fmt.Println(field, ":  ", value)
+	// }
+
+}
