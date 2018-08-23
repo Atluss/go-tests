a6df069f58af102351eba5b2f3a098a4422c131d
Shokin Ilya <atlus3@mail.ru>
2018-08-23 22:41:39 +0300

    разные тесты

--- tests/main.go
+++ tests/main.go
@@ -4,12 +4,27 @@ import (
 	"fmt"
 )
 
-func main_t() {
+type testSctruct struct {
+	x int
+}
 
+func (test *testSctruct) test() {
+
+}
+
+func main() {
+
+	x := [5]int{1, 3, 5, 6, 8}
+
+	for i, value := range x {
+		fmt.Println(i, " -- ", value)
+	}
+}
+
+func zeroExptionTest() {
 	for i := 0; i < 10; i++ {
 
 		fmt.Println("i: ", i, " ", i%2)
 
 	}
-
 }
