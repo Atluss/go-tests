186a1da987b3643936ecccd28f4aabc998976b4c
Shokin Ilya <atlus3@mail.ru>
2018-09-23 22:17:55 +0300

    time test

---
+++ time/main.go
@@ -0,0 +1,23 @@
+import (
+	"strings"
+	"time"
+)
+
+const DateTimeFormat = "2006-01-02T15:04:05.999999999Z070"
+
+func convertStrToTime(target string) time.Time {
+	t, _ := time.ParseInLocation(DateTimeFormat, target, EUROPE_MOSCOW)
+	return t
+}
+
+func convertTimeToStr(target time.Time) string {
+
+	timeStr := target.Format(DateTimeFormat)
+
+	if target.Nanosecond()-1e6 == 0 {
+		timeStr = strings.Replace(timeStr, "+", ".000+", -1)
+	}
+
+	return timeStr
+
+}
